4a24bf387189038fd89b4a4a4cf976e3dc3d1a95
Yuta Hinokuma <higumachan@users.noreply.github.com>
2024-06-14 10:50:22 +0900

    implement anneal_peek.

--- src/lib.rs
+++ src/lib.rs
@@ -25,14 +25,29 @@ pub trait AnnealingState<G: Rng>: Sized + Clone + Debug {
     fn apply(&mut self, ctx: &mut Self::Context, op: &Self::Transition) -> Option<()>;
 }
 
+/// AnnealingStatePeeking is a trait to be implemented when the energy of the next state can be calculated efficiently without updating the state.
+pub trait AnnealingStatePeeking<G: Rng>: AnnealingState<G> {
+    /// Peek the energy of the state after applying the transition
+    fn peek_energy(
+        &self,
+        ctx: &Self::Context,
+        op: &Self::Transition,
+        current_energy: Self::Energy,
+    ) -> Option<Self::Energy>;
+}
+
+/// AnnealingStateBack is implemented when the state can be back processed more efficiently than clone.
 pub trait AnnealingStateBack<G: Rng>: AnnealingState<G> {
     type Restore;
 
+    /// Apply the transition and restore information for returning to the previous state
     fn apply_with_restore(
         &mut self,
         ctx: &mut Self::Context,
         op: &Self::Transition,
     ) -> Option<Self::Restore>;
+
+    /// Restore the state to the previous state
     fn back(&mut self, ctx: &mut Self::Context, restore: &Self::Restore);
 }
 
@@ -84,6 +99,9 @@ impl<G: Rng, S: AnnealingState<G>, C: Schedule> Annealer<G, S, C> {
 }
 
 impl<G: Rng, S: AnnealingStateBack<G>, C: Schedule> Annealer<G, S, C> {
+    /// Simulated Annealing algorithm
+    /// minimize f(x) where x is a state
+    /// Use BACK instead of CLONE when you want to abort and return to the state.
     pub fn anneal_back(&mut self, rng: &mut G) -> S {
         let mut best_state = self.state.clone();
         let mut best_energy = self.state.energy();
@@ -100,6 +118,39 @@ impl<G: Rng, S: AnnealingStateBack<G>, C: Schedule> Annealer<G, S, C> {
                 if delta.is_sign_positive() && (-delta / temperature).exp() > p {
                     self.state.back(&mut self.ctx, &restore);
                 } else {
+                    current_energy = new_energy;
+                    if current_energy < best_energy {
+                        best_energy = current_energy;
+                        best_state = self.state.clone();
+                    }
+                }
+            }
+            progress.update();
+        }
+
+        best_state
+    }
+}
+
+impl<G: Rng, S: AnnealingStatePeeking<G>, C: Schedule> Annealer<G, S, C> {
+    /// Simulated Annealing algorithm
+    /// minimize f(x) where x is a state
+    /// Use peek_energy instead of apply when the energy of the next state can be calculated efficiently without updating the state.
+    pub fn anneal_peek(&mut self, rng: &mut G) -> S {
+        let mut best_state = self.state.clone();
+        let mut best_energy = self.state.energy();
+        let mut current_energy = best_energy;
+        let mut progress = Progress::zero();
+
+        while self.schedule.should_continue(&progress) {
+            let op = Transition::choose(rng, &self.ctx);
+            if let Some(new_energy) = self.state.peek_energy(&self.ctx, &op, current_energy) {
+                let temperature = self.schedule.temperature(&progress);
+                let delta = (new_energy - current_energy).into();
+                let p = rng.gen_range(0.0..=1.0);
+                if !(delta.is_sign_positive() && (-delta / temperature).exp() > p) {
+                    // accept
+                    self.state.apply(&mut self.ctx, &op);
                     current_energy = new_energy;
                     if current_energy < best_energy {
                         best_energy = current_energy;
